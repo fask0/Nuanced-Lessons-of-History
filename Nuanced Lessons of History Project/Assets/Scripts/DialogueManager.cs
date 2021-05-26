e0e86a400b1c332b1963327847c9cb97c502e289
Vasil Vasilev <fask0_og@hotmail.com>
2021-05-26 09:53:10 +0300

    Support for rich text tags Small improvement for character images

--- Nuanced Lessons of History Project/Assets/Scripts/DialogueManager.cs
+++ Nuanced Lessons of History Project/Assets/Scripts/DialogueManager.cs
@@ -11,11 +11,12 @@ public class DialogueManager : Singleton<DialogueManager>
     #region Speech Panel
     [Header("Speech Panel")]
     [SerializeField] private GameObject _speechPanel;
-    [SerializeField] private Image[] _characterImages;
+    [SerializeField] private GameObject _charactersContainer;
     [SerializeField] private TextMeshProUGUI _speechNameText;
     [SerializeField] private LocalizeStringEvent _speechText;
     [SerializeField] private Button _speechBoxButton;
     [SerializeField] private GameObject _clickToContinue;
+    private Image[] _characterImages;
     #endregion
 
     #region Dialogue
@@ -43,6 +44,10 @@ public class DialogueManager : Singleton<DialogueManager>
     {
         _speechNameText.text = "";
         _speechText.GetComponent<TextMeshProUGUI>().text = "";
+
+        _characterImages = new Image[_charactersContainer.transform.childCount];
+        for (int i = 0; i < _charactersContainer.transform.childCount; i++)
+            _characterImages[i] = _charactersContainer.transform.GetChild(i).GetComponent<Image>();
     }
 
     public void StartNewDialogue(DialogueScriptableObject pDialogue)
@@ -92,11 +97,10 @@ public class DialogueManager : Singleton<DialogueManager>
         while (!operation.IsDone)
             yield return null;
         string[] targetText = operation.Result.Split(new string[] { "<size=0>|</size>" }, System.StringSplitOptions.None);
-
         if (targetText.Length <= 1)
             _speechBoxButton.onClick.AddListener(_nextLineAction);
 
-        bool wantToQuickComplete = false;
+        bool shouldQuickComplete = false;
         UnityAction completeOnClickAction = () => completeOnClick();
         for (int i = 0; i < targetText.Length; i++)
         {
@@ -106,11 +110,13 @@ public class DialogueManager : Singleton<DialogueManager>
 
                 UnityAction onClickAction = () => onClick();
                 _speechBoxButton.onClick.AddListener(onClickAction);
+                _clickToContinue.SetActive(true);
 
                 while (!hasClicked)
                     yield return new WaitForEndOfFrame();
 
                 _speechBoxButton.onClick.RemoveListener(onClickAction);
+                _clickToContinue.SetActive(false);
 
                 #region Local Methods
                 void onClick()
@@ -120,14 +126,41 @@ public class DialogueManager : Singleton<DialogueManager>
                 #endregion
             }
 
-            _speechBoxButton.onClick.AddListener(completeOnClickAction);
-            for (int k = 0; k < targetText[i].Length; k++)
+            string currentText = "";
+            string[] speechAndTags = targetText[i].Split(new char[2] { '<', '>' });
+            for (int k = 0; k < speechAndTags.Length; k++)
             {
-                speechText.text += targetText[i][k];
-                if (!wantToQuickComplete) yield return new WaitForSeconds(delay);
+                string section = speechAndTags[k];
+                bool isTag = (k & 1) != 0;
+
+                if (isTag)
+                {
+                    currentText = speechText.text;
+                    ENCAPSULATED_TEXT encapsulation = new ENCAPSULATED_TEXT(string.Format("<{0}>", section), speechAndTags, k);
+                    _speechBoxButton.onClick.AddListener(completeOnClickAction);
+                    while (!encapsulation.IsDone)
+                    {
+                        bool hasStepped = encapsulation.Step();
+                        speechText.text = currentText + encapsulation.DisplayText;
+                        if (hasStepped)
+                            if (!shouldQuickComplete) yield return new WaitForSeconds(delay);
+                    }
+                    _speechBoxButton.onClick.RemoveListener(completeOnClickAction);
+                    shouldQuickComplete = false;
+                    k = encapsulation.SpeechAndTagsProgress + 1;
+                }
+                else
+                {
+                    _speechBoxButton.onClick.AddListener(completeOnClickAction);
+                    for (int j = 0; j < section.Length; j++)
+                    {
+                        speechText.text += section[j];
+                        if (!shouldQuickComplete) yield return new WaitForSeconds(delay);
+                    }
+                    _speechBoxButton.onClick.RemoveListener(completeOnClickAction);
+                    shouldQuickComplete = false;
+                }
             }
-            _speechBoxButton.onClick.RemoveListener(completeOnClickAction);
-            wantToQuickComplete = false;
         }
 
         finishSpeak();
@@ -135,7 +168,7 @@ public class DialogueManager : Singleton<DialogueManager>
         #region Local Methods
         void completeOnClick()
         {
-            wantToQuickComplete = true;
+            shouldQuickComplete = true;
         }
         #endregion
     }
@@ -173,23 +206,14 @@ public class DialogueManager : Singleton<DialogueManager>
 
     private bool progressDialogue()
     {
-        if (_dialogueProgress + 1 < _currentDialogue.Lines.Length)
-        {
-            _dialogueProgress++;
-            return true;
-        }
-        else
-        {
-            //End of dialogue
-            endDialogue();
-            return false;
-        }
+        if (_dialogueProgress + 1 < _currentDialogue.Lines.Length) { _dialogueProgress++; return true; }
+        else { endDialogue(); return false; }
     }
 
-    private void regressDialogue()
+    private bool regressDialogue()
     {
-        if (_dialogueProgress - 1 >= 0)
-            _dialogueProgress--;
+        if (_dialogueProgress - 1 >= 0) { _dialogueProgress--; return true; }
+        else return false;
     }
 
     private void endDialogue()
@@ -197,4 +221,120 @@ public class DialogueManager : Singleton<DialogueManager>
         _speechPanel.SetActive(false);
     }
     #endregion
+
+    private class ENCAPSULATED_TEXT
+    {
+        #region Fields
+        private string _tag = "";
+        private string _endTag = "";
+        private string _currentText = "";
+        private string _targetText = "";
+        private string _displayText = "";
+        private bool _isDone = false;
+        private string[] _speechAndTags = null;
+        private int _speechAndTagsProgress = 0;
+        private ENCAPSULATED_TEXT _encapsulator = null;
+        private ENCAPSULATED_TEXT _subEncapsulator = null;
+        #endregion
+
+        #region Properties
+        public string DisplayText => _displayText;
+        public bool IsDone => _isDone;
+        public int SpeechAndTagsProgress => _speechAndTagsProgress;
+        private ENCAPSULATED_TEXT Encapsulator { get => _encapsulator; set => _encapsulator = value; }
+        private ENCAPSULATED_TEXT SubEncapsulator { get => _subEncapsulator; set => _subEncapsulator = value; }
+        #endregion
+
+        #region Methods
+        public ENCAPSULATED_TEXT(string pTag, string[] pSpeechAndTags, int pSpeechAndTagsProgress)
+        {
+            _tag = pTag;
+            generateEndTag();
+            _speechAndTags = pSpeechAndTags;
+            _speechAndTagsProgress = pSpeechAndTagsProgress;
+
+            if (_speechAndTags.Length - 1 <= _speechAndTagsProgress) return;
+
+            string nextPart = _speechAndTags[_speechAndTagsProgress + 1];
+            _targetText = nextPart;
+            _speechAndTagsProgress++;
+        }
+
+        private void generateEndTag()
+        {
+            _endTag = _tag.Replace("<", "").Replace(">", "");
+
+            if (_endTag.Contains("="))
+                _endTag = string.Format("</{0}>", _endTag.Split('=')[0]);
+            else
+                _endTag = string.Format("</{0}>", _endTag);
+        }
+
+        public bool Step()
+        {
+            if (_isDone) return true;
+            if (_subEncapsulator != null && !_subEncapsulator.IsDone) return _subEncapsulator.Step();
+            else
+            {
+                if (_currentText == _targetText)
+                {
+                    if (_speechAndTags.Length > _speechAndTagsProgress + 1)
+                    {
+                        string nextPart = _speechAndTags[_speechAndTagsProgress + 1];
+                        bool isTag = ((_speechAndTagsProgress + 1) & 1) != 0;
+                        if (isTag)
+                        {
+                            if (string.Format("<{0}>", nextPart) == _endTag)
+                            {
+                                _isDone = true;
+                                if (_encapsulator != null)
+                                {
+                                    string taggedText = (_tag + _currentText + _endTag);
+                                    _encapsulator._currentText += taggedText;
+                                    _encapsulator._targetText += taggedText;
+                                    updateArrayProgress(2);
+                                }
+                            }
+                            else
+                            {
+                                _subEncapsulator = new ENCAPSULATED_TEXT(string.Format("<{0}>", nextPart), _speechAndTags, _speechAndTagsProgress + 1) { Encapsulator = this };
+                                updateArrayProgress();
+                            }
+                        }
+                        else
+                        {
+                            _targetText += nextPart;
+                            updateArrayProgress();
+                        }
+                    }
+                    else _isDone = true;
+                }
+                else
+                {
+                    _currentText += _targetText[_currentText.Length];
+                    updateDisplay("");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void updateArrayProgress(int pValue = 1)
+        {
+            _speechAndTagsProgress += pValue;
+
+            if (_encapsulator != null)
+                _encapsulator.updateArrayProgress(pValue);
+        }
+
+        private void updateDisplay(string pSubText)
+        {
+            _displayText = string.Format("{0}{1}{2}{3}", _tag, _currentText, pSubText, _endTag);
+
+            if (_encapsulator != null)
+                _encapsulator.updateDisplay(_displayText);
+        }
+        #endregion
+    }
 }
